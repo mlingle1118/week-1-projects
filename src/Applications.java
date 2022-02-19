2e1d4a43b0f089f52a0302e217c56fddcb07bd5d
unknown <mlingle1118@gmail.com>
2022-02-18 18:40:51 -0600

    first commit being made

---
+++ src/Applications.java
@@ -0,0 +1,17 @@
+
+public class Applications {
+
+	public static void main(String[] args) {
+		int samsAge =67;
+		double accountBalance = 34.67;
+		char middleInitial = 'C';
+		boolean isHotOutside = true;
+		String firstName = "Sam";
+		System.out.println(samsAge);
+	    System.out.println(accountBalance);
+	    System.out.println (middleInitial);
+	    System.out.println (isHotOutside);
+	    System.out.println (firstName);
+	}
+
+}
